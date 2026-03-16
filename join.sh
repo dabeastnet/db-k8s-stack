981c8ca9005476c129357357d4b0486fad2fba44
Dieter Beckers <dabeastmods@gmail.com>
2026-03-16 21:58:56 +0100

    huarh

--- join.sh
+++ join.sh
@@ -1,1 +1,1 @@
-kubeadm join 192.168.56.10:6443 --token nqh28u.r4g6pkqal8mhxoxa --discovery-token-ca-cert-hash sha256:d637da91da3579319d2924cfef1bf62b67579bc232259f2e572a21bcdc35a063 
+kubeadm join 192.168.56.10:6443 --token jpt16u.waeadrij47q9fj5u --discovery-token-ca-cert-hash sha256:464ebcf204244e4f27dd82c2ee83a3bbe33fb158e5073abc410fb846cf51ffd2 
